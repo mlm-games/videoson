7b645caffd0992ae77a5c7d598a868a8a6e1b72b
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-05-26 20:47:38 +0530

    misc api change

--- videoson/src/lib.rs
+++ videoson/src/lib.rs
@@ -3,8 +3,20 @@
 pub use videoson_core::{
     CodecType, Packet, PixelFormat, PlaneData, Result, VideoCodecParams, VideoDecoder,
     VideoDecoderOptions, VideoFrame, VideoFramePlanes, VideoPlane, VideosonError,
-    CodecRegistry, RegisterableVideoDecoder, SupportedVideoCodec,
+    CodecRegistry, RegisterableVideoDecoder, SupportedVideoCodec, NalFormat,
 };
+
+#[cfg(feature = "h264")]
+pub use videoson_codec_h264 as codec_h264;
+
+#[cfg(feature = "h264")]
+pub use videoson_codec_h264::H264Decoder;
+
+#[cfg(feature = "rav1d")]
+pub use videoson_codec_rav1d as codec_rav1d;
+
+#[cfg(feature = "rav1d")]
+pub use videoson_codec_rav1d::Rav1dSafeDecoder;
 
 pub mod prelude {
     pub use videoson_core::{
